d952b1b32930aef06f1758865af3b2c4dcd43b1f
ptmi <ptomi2803@gmail.com>
2017-05-02 08:37:18 +0200

    asd

---
+++ app/src/main/java/com/example/debre/beesmarterbeesgame/DeathActivity.java
@@ -0,0 +1,77 @@
+package com.example.debre.beesmarterbeesgame;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class DeathActivity extends AppCompatActivity {
+
+    Button button;
+    TextView textView;
+    TextView textView2;
+    GamePanel gamePanel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_death);
+
+        int mUIFlag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+        getWindow().getDecorView().setSystemUiVisibility(mUIFlag);
+        final Intent i = new Intent(DeathActivity.this, MainActivity.class);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView.setTextSize(40);
+
+
+        SharedPreferences settings = getSharedPreferences("GAME_DATA", Context.MODE_PRIVATE);
+        int highScore = settings.getInt("HIGH_SCORE", 0);
+        if (gamePanel.score > highScore) {
+            textView.setText(String.valueOf(gamePanel.score));
+            textView2.setText(String.valueOf(gamePanel.score));
+
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putInt("HIGH_SCORE", gamePanel.score);
+            editor.commit();
+
+        } else {
+            textView.setText(String.valueOf(highScore));
+            textView2.setText(String.valueOf(gamePanel.score));
+
+        }
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(i);
+            }
+        });
+
+    }
+
+
+
+
+
+
+}
